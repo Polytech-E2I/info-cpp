255284d7b75cc5a6b4ab6b191d9f2e4cea111276
Robin SHAMSNEJAD <rsham@free.fr>
2021-09-30 12:04:44 +0200

    Prettify output

--- TP01/Exo02/main.cpp
+++ TP01/Exo02/main.cpp
@@ -1,17 +1,25 @@
 #include <cstdlib>
+#include <iostream>
 #include "matrix/matrix.h"
 
 int main(void)
 {
     Matrix matrix{};
 
+    std::cout << "==== DÉFINITION DE LA MATRICE ====\n";
     matrix.fill();
+
+    std::cout << "\n";
+
+    std::cout << "==== MATRICE ====\n";
     matrix.print();
 
     matrix.transpose();
+    std::cout << "==== MATRICE TRANSPOSÉE ====\n";
     matrix.print();
 
     matrix.transpose();
+    std::cout << "==== MATRICE COMME VECTEUR UNIDIMENSIONNEL ====\n";
     matrix.print(Matrix::unidimensional);
 
     return EXIT_SUCCESS;
